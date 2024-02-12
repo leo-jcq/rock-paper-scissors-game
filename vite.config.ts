b8b32b1a425d79a53bc68dcfb837c018cba2d679
Léo Jacquet <leojacquet2003@gmail.com>
2024-02-12 21:58:22 +0100

    🚨 fix base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: 'rock-paper-scissors-game'
+    base: '/rock-paper-scissors-game'
 });
