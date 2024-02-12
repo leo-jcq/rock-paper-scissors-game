2b47a2158d8084b4af187ee1cd9bebe4e76284b2
Léo Jacquet <leojacquet2003@gmail.com>
2024-02-12 22:01:12 +0100

    🔧 add path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import react from '@vitejs/plugin-react-swc';
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/rock-paper-scissors-game'
+    base: '/rock-paper-scissors-game',
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    }
 });
