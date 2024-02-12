5f2a7d891718ada66a5d189939c29a20459af8ba
Léo Jacquet <leojacquet2003@gmail.com>
2024-02-12 21:52:04 +0100

    🏗️ organize files

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react';
+import { FC, useState } from 'react';
 import './App.css';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 
-function App() {
+const App: FC = () => {
     const [count, setCount] = useState(0);
 
     return (
@@ -26,6 +26,6 @@ function App() {
             <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
         </>
     );
-}
+};
 
 export default App;
